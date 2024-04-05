6d533db61a1c966fde5d047fc6ce71277747ffbc
anttont <anttoni.tornikoski@gmail.com>
2024-04-05 22:02:35 +0300

    Navbar logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,13 +9,26 @@ const Navbar = () => {
 const [active, setActive] = useState(" ")
 
   return (
-    <nav className={`${styles.paddingX} w-full flex-items-center py-5 fixed top-0 z-20 bg-primary`}>
+    <nav className={`${
+      styles.paddingX
+    } w-full flex items-center py-5 fixed top-0 z-20 "bg-primary"  
+    }`}
+  >
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
-        <Link to="/" className="flex items-center gap-2" onClick={() => {setActive(" "); window.scrollTo(0, 0)}} >
-          <img src={logo} alt="logo" className="w-9 h-9 object-contain"/>
-          <p className='text-white text-[18px] font-bold cursor-pointer'>Anttoni 
-          <span className='sm: block hidden'>| Tornikoski</span></p>
-          </Link> 
+      <Link
+          to='/'
+          className='flex items-center gap-2'
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt='logo' className='w-9 h-9 object-contain' ></img>
+          <p className='text-white text-[18px] font-bold cursor-pointer flex '>
+            Anttoni&nbsp;
+            <span className='sm:block hidden'> | Tornikoski</span>
+          </p>
+        </Link>
       </div>
     </nav>
   )
